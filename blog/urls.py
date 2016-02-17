0e6a8f8f1235a47795c415c6c1184097ddeff952
David Lance <dcl71@case.edu>
2016-02-17 15:13:30 -0500

    urls are working

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^blog/(?P<year>[0-9]{4})/$', views.year_archive),
-    url(r'^blog/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.month_archive),
-    url(r'^blog/post/(?P<post_id>[0-9]+)/$', views.post),
+    url(r'^(?P<year>[0-9]{4})/$', views.year_archive, name='year_archive'),
+    url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.month_archive, name='month_archive'),
+    url(r'^post/(?P<post_id>[0-9]+)/$', views.post, name='post'),
     url(r'^$', views.post_list, name='post_list'),    
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,15 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post(request, post_id):
+    posts = Post.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+def year_archive(request, year):
+    posts = Post.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+def month_archive(request, year, month):
+    posts = Post.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
