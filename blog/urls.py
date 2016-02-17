4d36f5bcc97e1dd535c6b1292ce1c5fe91979963
David Lance <dcl71@case.edu>
2016-02-17 14:40:45 -0500

    Initial post sorting for urls

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^blog/(?P<year>[0-9]{4})/$', views.year_archive),
+    url(r'^blog/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.month_archive),
+    url(r'^blog/post/(?P<post_id>[0-9]+)/$', views.post),
     url(r'^$', views.post_list, name='post_list'),    
 
 ]
