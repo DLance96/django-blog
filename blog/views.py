e70c9c4876f4e3445fb43bf24bb75d56f45b9cae
David Lance <dcl71@case.edu>
2016-02-17 15:38:56 -0500

    Added post archive sorting

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -7,13 +7,13 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post(request, post_id):
-    posts = Post.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')
+    posts = Post.objects.filter(pk=post_id)
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def year_archive(request, year):
-    posts = Post.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')
+    posts = Post.objects.filter(pub_date__year = year).order_by('pub_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def month_archive(request, year, month):
-    posts = Post.objects.filter(pub_date__lte=timezone.now()).order_by('pub_date')
+    posts = Post.objects.filter(pub_date__month = month, pub_date__year = year).order_by('pub_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
