b52523b78b7ebc5358cb3dc9aa257cc5b3fbbb72
David Lance <dcl71@case.edu>
2016-02-24 17:24:36 -0500

    Remove tags and fixed .gitignore

--- blog/models.py
+++ blog/models.py
@@ -3,14 +3,8 @@ from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    tags = models.CharField(max_length=200)
     pub_date = models.DateTimeField(blank=True, null=True)
     text = models.TextField()
-
-    def publish(self):
-        self.pub_date = timezone.now()
-        self.save()
 
     def __str__(self):
         return self.title
