631272ee1fab37994735e263ef1ae1f523989a8e
David Lance <dcl71@case.edu>
2016-02-25 15:16:42 -0500

    Removed contact page and added contact button

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     url(r'^(?P<year>[0-9]{4})/$', views.year_archive, name='year_archive'),
     url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.month_archive, name='month_archive'),
     url(r'^post/(?P<post_id>[0-9]+)/$', views.post, name='post'),
-    url(r'^contact$', views.contact, name='contact'),
     url(r'^$', views.post_list, name='post_list'),    
 ]
