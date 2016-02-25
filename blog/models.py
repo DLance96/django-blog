16e28e08506baa03471e092177edb6bd298bacc8
David Lance <dcl71@case.edu>
2016-02-25 15:33:40 -0500

    added post desciptions and usage

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField(blank=True, null=True)
+    description = models.CharField(max_length=200, default="")
     text = models.TextField()
 
     def __str__(self):
