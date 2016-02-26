70be1a9a02a9a686923b24286598d9df6e7c0bf0
David Lance <dcl71@case.edu>
2016-02-26 12:06:48 -0500

    Removed text field from post and shifted towards using section system

--- blog/models.py
+++ blog/models.py
@@ -5,18 +5,17 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField(blank=True, null=True)
     description = models.CharField(max_length=200, default="")
-    text = models.TextField()
 
     def __str__(self):
         return self.title
 
 def post_dir_path(instance, filename):
-    return 'media/post/{0}/{1}_{2}'.format(instance.post.id, instance.id, filename)
+    return 'post/{0}/{1}_{2}'.format(instance.post.id, instance.id, filename)
 
 class Section(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    img = models.ImageField(upload_to=post_dir_path)
+    img = models.ImageField(upload_to=post_dir_path, default="")
     text = models.TextField()
     
     def __str__(self):
