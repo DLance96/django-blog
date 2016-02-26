c5e4989337250b1da97cc467303804cd130e6506
David Lance <dcl71@case.edu>
2016-02-25 20:07:05 -0500

    Changed up images for pages

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^(?P<year>[0-9]{4})/$', views.year_archive, name='year_archive'),
     url(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.month_archive, name='month_archive'),
     url(r'^post/(?P<post_id>[0-9]+)/$', views.post, name='post'),
+    url(r'^about$', views.about, name='about'),
     url(r'^$', views.post_list, name='post_list'),    
 ]
--- blog/views.py
+++ blog/views.py
@@ -17,3 +17,6 @@ def year_archive(request, year):
 def month_archive(request, year, month):
     posts = Post.objects.filter(pub_date__month = month, pub_date__year = year).order_by('pub_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def about(request):
+    return render(request, 'blog/about.html', {})
