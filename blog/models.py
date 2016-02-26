9b46764d2a993d4f70dc20c29fc3770e6bb27ead
David Lance <dcl71@case.edu>
2016-02-26 00:04:25 -0500

    Added image uploading but not displaying

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+def post_dir_path(instance, filename):
+    return 'media/post/{0}/{1}_{2}'.format(instance.post.id, instance.id, filename)
+
+class Section(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    img = models.ImageField(upload_to=post_dir_path)
+    text = models.TextField()
+    
+    def __str__(self):
+        return self.title
+
